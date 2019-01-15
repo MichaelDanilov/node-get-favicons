32cd5b23ff8d9119ad9b174b2b975424e0649441
Michael Danilov <michael@danilov.me>
2019-01-15 14:14:37 +0600

    Mock real requests inside tests

--- test/index.js
+++ test/index.js
@@ -1,5 +1,9 @@
 const fs = require('fs');
 const test = require('ava');
+const axios = require('axios');
+const MockAdapter = require('axios-mock-adapter');
+
+const mock = new MockAdapter(axios);
 
 const getFavicons = require('../index');
 
@@ -8,8 +12,9 @@ test('empty host', async (t) => {
   t.is(error.message, 'Host must be non-zero string');
 });
 
-// let's hope that example.com has never changed
 test('get example.com', async (t) => {
+  mock.onGet('https://example.com').replyOnce(200, '');
+
   const favicons = await getFavicons.byUrl('https://example.com');
   t.deepEqual(favicons, []);
 });
@@ -51,16 +56,50 @@ test('read yandex.com', async (t) => {
 });
 
 test('get mobile.twitter.com', async (t) => {
+  mock.onGet('https://mobile.twitter.com').replyOnce(200, fs.readFileSync('./test/mobile-twitter-com.html', 'utf8'));
+  mock.onGet('https://mobile.twitter.com/manifest.json').replyOnce(200, fs.readFileSync('./test/mobile-twitter-com-manifest-json.html', 'utf8'));
+
   const favicons = await getFavicons.byUrl('https://mobile.twitter.com');
+
   t.deepEqual(favicons, [{
+    type: 'mask-icon',
+    imgType: null,
+    href: 'https://abs.twimg.com/responsive-web/web/icon-svg.9e211f626ec9d281.svg',
+    sizes: null,
+    colors: 'any',
+  }, {
     type: 'icon',
     imgType: 'image/x-icon',
-    href: 'https://ma.twimg.com/twitter-mobile/33f19cd501f08efb0522b6e1acfe25fdcbf618be/images/favicon.ico',
+    href: 'https://abs.twimg.com/favicons/favicon.ico',
     sizes: null,
+  }, {
+    type: 'apple-touch-icon',
+    imgType: null,
+    href: 'https://abs.twimg.com/responsive-web/web/icon-ios.8ea219d08eafdfa4.png',
+    sizes: '192x192',
+  }, {
+    type: 'theme-color',
+    imgType: null,
+    href: null,
+    sizes: null,
+    content: '#ffffff',
+  }, {
+    type: 'manifest',
+    imgType: 'image/png',
+    href: 'https://abs.twimg.com/responsive-web/web/icon-default.604e2486a34a2f6e.png',
+    sizes: '192x192',
+  }, {
+    type: 'manifest',
+    imgType: 'image/png',
+    href: 'https://abs.twimg.com/responsive-web/web/icon-default.604e2486a34a2f6e.png',
+    sizes: '512x512',
   }]);
 });
 
 test('get pwa.rocks', async (t) => {
+  mock.onGet('https://pwa.rocks').replyOnce(200, fs.readFileSync('./test/pwa-rocks.html', 'utf8'));
+  mock.onGet('https://pwa.rocks/pwa.webmanifest').replyOnce(200, fs.readFileSync('./test/pwa-rocks-pwa-webmanifest.html', 'utf8'));
+
   const favicons = await getFavicons.byUrl('https://pwa.rocks');
   t.deepEqual(favicons, [{
     type: 'icon',
